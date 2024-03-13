c9a9b0953629e13e14cef3482ec522a21ab08abf
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2024-03-13 22:49:34 +0700

    размер карточек v6

--- resources/views/schools/index.blade.php
+++ resources/views/schools/index.blade.php
@@ -236,7 +236,7 @@ footer {
     <div class="col-3  p-8" style="height: 400px;">
         <div class="card text-center p-3">
             
-            <img class="img-fluid" style="height: 300px;" src="{{asset('storage/img/categori-img/' . $school->desk)}}" alt="{{$school->desk}}">
+            <img class="img-fluid" style="height: 200px;" src="{{asset('storage/img/categori-img/' . $school->desk)}}" alt="{{$school->desk}}">
             <div class="card-header p-3">
                 <h3>{{$school->name}}</h3>
             </div>
