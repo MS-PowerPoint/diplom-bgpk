7d575ae3e384568f560d9d189b463db19415739c
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2024-03-14 00:57:46 +0700

    перенос css в наследуемый шаблон

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -8,7 +8,241 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
 
+    <style>
+    * {
 
+    padding: 0px;
+    margin: 0px auto;
+    font-family: 'Sedgwick Ave Display', cursive;
+    color: rgb(186, 186, 186);
+    text-shadow: black 1px 0 3px,
+    black -1px 0 3px,
+    black 0px 1px 3px,
+    black 0px -1px 3px;
+}
+a{
+    text-decoration: none;
+
+}
+
+
+body {
+top: 0 !important;
+background-color: rgb(227, 227, 227);
+}
+
+.skiptranslate {
+    display: none;
+    height: 0;
+}
+
+.language__img {
+    cursor: pointer;
+}
+
+
+    header {
+    background-color: rgb(50, 50, 50);
+
+}
+
+.logo {
+    width: 40%;
+}
+
+.menu-content {
+    max-height: 0;
+    overflow: hidden;
+    font-family: 'Oswald', sans-serif;
+
+}
+
+li a:hover {
+    color: red;
+    background-color: rgb(30, 30, 30);
+}
+
+.menu-content li {
+    width: 16.6%;
+}
+
+input:checked~label {
+    background-image: url(/menu-png/cross.png);
+}
+
+input:checked~.menu-content {
+    max-height: 100%;
+}
+
+.collapsible-menu {
+
+
+    border-bottom: 3px solid black;
+
+
+}
+
+.collapsible-menu ul {
+    border-top: 3px solid black;
+    display: flex;
+    list-style-type: none;
+    padding: 0;
+
+}
+
+.collapsible-menu a {
+    display: block;
+    color: rgb(186, 186, 186);
+    text-decoration: none;
+}
+
+
+.collapsible-menu label {
+
+    font-size: 20px;
+    margin: 1px;
+    display: block;
+    cursor: pointer;
+    background: url(/menu-png/menu-bar.png) no-repeat center;
+    background-size: 30px 80%;
+    background-position: 0;
+    width: 90px;
+
+
+    padding: 5px
+}
+
+.collapsible-menu label:hover {
+    color: red;
+
+}
+
+input#menu {
+    display: none;
+}
+
+input:checked+label {
+    background-image: url(/menu-png/cross.png);
+}
+
+.card-header h4{
+    /* text-shadow: none; */
+    text-shadow: black 1px 0 1px,
+    black -1px 0 1px,
+    black 0px 1px 1px,
+    black 0px -1px 1px;
+}
+
+footer {
+    width: 100%;
+    background-color: rgb(50, 50, 50);
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    padding: 10px 0;
+
+
+}
+    
+.menu-footer {
+    max-height: 100%;
+    text-align: center;
+    margin: 0 15px;
+
+
+}
+
+.menu-footer a {
+    font-size: 15px;
+    text-decoration: none;
+    color: rgb(186, 186, 186);
+
+}
+
+.menu-footer Label {
+
+    font-size: 30px;
+}
+
+.menu-footer ul {
+    list-style-type: none;
+    padding: 0px;
+
+}
+
+.Payment-method-footer {
+
+    margin: 10px;
+
+}
+
+.Connection-method-footer img {
+    width: 10%;
+}
+
+.Payment-method-footer img {
+    width: 15%;
+}
+
+.footer-column {
+    display: flex;
+    width: 33%;
+    align-items: center;
+    justify-content: center;
+
+}
+
+.footer-column.two {
+
+
+    padding-top: 20px;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+
+
+
+/* стили для кнопки up */
+.btn-up {
+    position: fixed;
+    background-color: grey;
+    left: 20px;
+    bottom: 20px;
+    border-radius: 30px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: opacity 0.3s ease-in-out;
+    width: 50px;
+    height: 50px;
+    opacity: 1;
+}
+
+.btn-up::before {
+    content: "";
+    width: 40px;
+    height: 40px;
+    background-size: 100% 100%;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='%23fff' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' d='M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z'/%3E%3C/svg%3E");
+}
+
+.btn-up_hide {
+    display: none;
+}
+
+.btn-up_hiding {
+    opacity: 0;
+}
+
+@media (hover: hover) and (pointer: fine) {
+    .btn-up:hover {
+        background-color: grey;
+    }
+}
+
+</style>
 </head>
 <body>
 <header>
