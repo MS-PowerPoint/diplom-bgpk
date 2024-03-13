d6cc2286e60dd5ef27d87dffa02c68fce83d681b
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2024-03-13 23:07:34 +0700

    размер карточек v10

--- resources/views/schools/index.blade.php
+++ resources/views/schools/index.blade.php
@@ -233,14 +233,14 @@ footer {
 <a href="/schools/create" class="btn btn-primary" >CREATE💹</a>
 <div class="row">
     @foreach($schools as $school)
-    <div class="col-3 m-6" style="height: 400px;">
-        <div class="card text-center p-3">
-            
-            <img class="img-fluid" style="height: 200px;" src="{{asset('storage/img/categori-img/' . $school->desk)}}" alt="{{$school->desk}}">
-            <div class="card-header p-3">
-                <h2>{{$school->name}}</h2>
-            </div>
-        </div>
+
+        
+<div class="card" style="width: 18rem;">
+<img src="{{asset('storage/img/categori-img/' . $school->desk)}}" class="card-img-top" alt="{{$school->desk}}">
+<div class="card-body">
+<p class="card-text">{{$school->name}}</p>
+</div>
+</div>
 
         <!-- <div class="body-body">
             <h5 class="card-title">{{$school->name}}</h5>
@@ -257,7 +257,7 @@ footer {
 
         </div> -->
         
-    </div>   
+    
     @endforeach
 </div>
 @endsection
