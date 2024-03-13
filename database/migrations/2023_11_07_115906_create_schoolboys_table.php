df0b8bc2d9a7e68d382b21b76003bee042c90a89
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2024-03-14 01:45:23 +0700

    fix

--- database/migrations/2023_11_07_115906_create_schoolboys_table.php
+++ database/migrations/2023_11_07_115906_create_schoolboys_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('class');
             $table->string('estimation');
             $table->foreignId('school_id');
-            $table->string('img');
+            $table->string('img')->default(1);
             
 
         });
