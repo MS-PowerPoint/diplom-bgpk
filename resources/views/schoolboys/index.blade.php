d633112162207ccf2686a8eaf75ad453a1949ca4
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2024-03-14 02:52:37 +0700

    fix

--- resources/views/schoolboys/index.blade.php
+++ resources/views/schoolboys/index.blade.php
@@ -6,19 +6,23 @@
 <a href="/" class="btn btn-primary" >☜(ﾟヮﾟ☜)BACK</a>
 <a href="/schoolboys/create" class="btn btn-primary" >CREATE💹</a>
 <div class="row">
+<div class="row row-cols-1 row-cols-md-3 g-4">
     @foreach($schoolboys as $schoolboy)
 
-<div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card h-100">
       <img src="{{asset('storage/img/space3/' . $schoolboy->img)}}" class="card-img-top" alt="{{$schoolboy->img}}">
       <div class="card-body">
+
         <h5 class="card-title">{{$schoolboy->name}}</h5>
+        <p class="card-text">{{$schoolboy->school->name}}</p>
         <p class="card-text">{{$schoolboy->desk}}</p>
+
+        <a href="schoolboys/{{$schoolboy->id}}" class="btn btn-primary" > Подробнее😲</a>
       </div>
     </div>
   </div>
-</div>
+
 
 
     <!-- <div class="col-6  p-3">
@@ -28,7 +32,6 @@
         </div>
 
         <div class="body-body">
-            <a href="schoolboys/{{$schoolboy->id}}" class="btn btn-primary" > Подробнее😲</a>
             <a href="schoolboys/{{$schoolboy->id}}/update" class="btn btn-primary" > Изменить✍</a>
             
             <form action="{{ route('schoolboy.delete', ['schoolboy' => $schoolboy->id]) }}" method="post">
@@ -42,5 +45,6 @@
         </div>
     </div>    -->
     @endforeach
+</div>
 </div>
 @endsection
