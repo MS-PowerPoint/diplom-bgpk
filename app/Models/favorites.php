9eeae5344d8e835211438a305129b78bd1106f82
MS-PowerPoint <114092392+MS-PowerPoint@users.noreply.github.com>
2024-03-27 12:54:48 +0700

    fix

--- app/Models/favorites.php
+++ app/Models/favorites.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+protected $fillable=['name','surname','patronymic','class','estimation','school_id','img'];
 class favorites extends Model
 {
     use HasFactory;
--- database/migrations/2024_03_13_233249_create_favorites_table.php
+++ database/migrations/2024_03_13_233249_create_favorites_table.php
@@ -14,6 +14,13 @@ return new class extends Migration
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('patronymic');
+            $table->string('class');
+            $table->string('estimation');
+            $table->foreignId('school_id');
+            $table->string('img');
         });
     }
 
